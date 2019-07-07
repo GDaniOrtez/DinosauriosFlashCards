f220559400a0ce348c3558ae8885ae0d9e84fb64
Genesis Ortez <gdaniortez@gmail.com>
2019-07-07 12:36:57 -0600

    Sexto Commit

--- app/src/main/java/com/example/dinosauriosflashcards/Card.java
+++ app/src/main/java/com/example/dinosauriosflashcards/Card.java
@@ -4,12 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Card extends AppCompatActivity {
 
     private ViewPager mSlideViewPager;
     private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
+
+    private SliderAdapter sliderAdapter;
+
+    private Button mNextBtn;
+    private Button mCloseBtn;
+
+    private int Numero(int max){
+
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
+
+        int numRandom = random.nextInt(max);
+        return numRandom;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +38,16 @@ public class Card extends AppCompatActivity {
 
         mSlideViewPager = (ViewPager) findViewById(R.id.sLideViewPager);
         mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
+
+        sliderAdapter = new SliderAdapter(this);
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator(0);
+
+        mNextBtn = (Button) findViewById(R.id.nextbtn);
+        mCloseBtn = (Button) findViewById(R.id.closebtn);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
     }
 
 
