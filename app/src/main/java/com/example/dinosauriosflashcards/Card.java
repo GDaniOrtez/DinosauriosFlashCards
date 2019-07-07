0ecd73ceb866bcf629484f6da4d6eeb751a2df1a
Genesis Ortez <gdaniortez@gmail.com>
2019-07-06 21:36:34 -0600

    Segundo Commit

---
+++ app/src/main/java/com/example/dinosauriosflashcards/Card.java
@@ -0,0 +1,16 @@
+package com.example.dinosauriosflashcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+    }
+
+
+}
