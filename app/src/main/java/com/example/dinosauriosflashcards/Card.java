37740e7c7603a0e841261e2bceecc96b6e53ab6f
Genesis Ortez <gdaniortez@gmail.com>
2019-07-06 21:50:50 -0600

    Segundo Commit

--- app/src/main/java/com/example/dinosauriosflashcards/Card.java
+++ app/src/main/java/com/example/dinosauriosflashcards/Card.java
@@ -1,15 +1,22 @@
 package com.example.dinosauriosflashcards;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Card extends AppCompatActivity {
 
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+
+        mSlideViewPager = (ViewPager) findViewById(R.id.sLideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
     }
 
 
