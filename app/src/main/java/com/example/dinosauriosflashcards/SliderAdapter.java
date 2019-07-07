38555953fb6a551ab84341eb0ed99b1b240f91b7
Genesis Ortez <gdaniortez@gmail.com>
2019-07-06 23:01:34 -0600

    Cuarto Commit

---
+++ app/src/main/java/com/example/dinosauriosflashcards/SliderAdapter.java
@@ -0,0 +1,60 @@
+package com.example.dinosauriosflashcards;
+
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+
+    public SliderAdapter(Context context)
+    {
+        this.context=context;
+    }
+
+
+    public int[] slide_images ={
+
+            R.mipmap.braquiosaurio,
+            R.mipmap.nbraquiosauurio,
+    };
+
+    @Override
+    public int getCount() {
+        return slide_images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == (RelativeLayout) o;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position){
+
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.slide_image);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object){
+        container.removeView((RelativeLayout)object);
+    }
+}
